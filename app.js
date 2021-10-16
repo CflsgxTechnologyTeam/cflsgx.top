02dcc49d8eff7f8ab9a72ebf0b61e23afbdc35b6
RiverTwilight <wrj2014@126.com>
2021-10-16 21:20:44 +0800

    refactor: use express

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+const router = express.Router();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const ROUTES_LIST = [
+	{
+		path: "/",
+		view: "index",
+		title: "CFLSGX团学会_成都外国语学校高新校区团学会",
+		description: "",
+	},
+	{},
+];
+
+ROUTES_LIST.forEach(({ path, view, ...attrs }) => {
+	router.get(path, function (req, res, next) {
+		res.type("html");
+		console.log({ attrs });
+		res.render(view, { ...attrs });
+	});
+});
+
+app.use("/", router);
+app.use(express.static("public"));
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
